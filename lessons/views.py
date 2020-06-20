c89459f420bbbb914bec8bbe5b71afd41d55ec66
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-20 21:27:26 +0200

    Initial views file with index and latest lessons functions added.

---
+++ lessons/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from .models import Lesson
+
+# Create your views here.
+def index(request):
+	return render(request, 'index.html', {})
+
+def latest_lessons(request):
+	latest_lessons = Lesson.objects.all()[:5]
+	context = {
+	    'latest_lessons':latest_lessons
+	}
+	return render(request, 'latest.html', context)
