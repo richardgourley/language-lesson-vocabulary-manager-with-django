c076efe17674e08a4612243b4e5bf11477f9efd4
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-21 13:52:10 +0200

    Admin changed to allow entries to be added for each lesson

--- lessons/admin.py
+++ lessons/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Lesson
+from .models import Lesson, Entry
 
 # Register your models here.
-admin.site.register(Lesson)
+class EntryInLine(admin.StackedInline):
+	model = Entry
+
+class LessonAdmin(admin.ModelAdmin):
+	fieldsets = [
+        (None, {'fields':['lesson_name','description','order']})
+	]
+	inlines = [EntryInLine]
+
+admin.site.register(Lesson, LessonAdmin)
