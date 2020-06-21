7a47b087010a26cb309df80a4778e3485a806a4a
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-21 11:55:02 +0200

    View all url added

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'lessons'
 urlpatterns = [
     path('', views.index, name="index"),
-    path('latest/', views.latest_lessons, name="latest_lessons")
+    path('latest/', views.latest_lessons, name="latest_lessons"),
+    path('viewall/', views.all_lessons, name="all_lessons")
 ]
