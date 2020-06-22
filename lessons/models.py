c4d1ee138095313248d6d89cbb89a5867b339fb3
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-22 20:20:31 +0200

    Update models added

--- lessons/models.py
+++ lessons/models.py
@@ -1,1 +1,18 @@
+from django.db import models
 
+# Create your models here.
+class Lesson(models.Model):
+	lesson_name = models.CharField(max_length=150)
+	description = models.TextField()
+	order = models.IntegerField(default=100)
+
+	def __str__(self):
+		return self.lesson_name
+
+class Entry(models.Model):
+	lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+	entry_text = models.CharField(max_length=250)
+	translation = models.CharField(max_length=250)
+
+	def __str__(self):
+	    return self.entry_text
