24795fa688de7b6c790928740b24b2d59d9f21e4
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-23 21:03:30 +0200

    Search entries replaces individual entries

--- lessons/views.py
+++ lessons/views.py
@@ -4,32 +4,39 @@ from .models import Lesson, Entry
 
 # Create your views here.
 def index(request):
-	return render(request, 'lessons/index.html', {})
+    return render(request, 'lessons/index.html', {})
 
 def latest_lessons(request):
-	latest_lessons = Lesson.objects.all()[:5]
-	context = {
-	    'latest_lessons':latest_lessons
-	}
-	return render(request, 'lessons/latest.html', context)
+    latest_lessons = Lesson.objects.all()[:5]
+    context = {
+        'latest_lessons':latest_lessons
+    }
+    return render(request, 'lessons/latest.html', context)
 
 def all_lessons(request):
-	all_lessons = Lesson.objects.all()
-	context = {
-	    'all_lessons':all_lessons
-	}
-	return render(request, 'lessons/all.html', context)
+    all_lessons = Lesson.objects.all()
+    context = {
+        'all_lessons':all_lessons
+    }
+    return render(request, 'lessons/all.html', context)
 
-def individual_entries(request):
-	all_entries = Entry.objects.order_by('entry_text')
-	context = {
-	    'all_entries':all_entries
-	}
-	return render(request, 'lessons/entries.html', context)
+def search_entries(request):
+    search_term = ''
+    search_result = ''
+    if request.POST:
+        search_term = request.POST['entry_search']
+    if search_term != '':
+            search_result = Entry.objects.filter(entry_text__icontains=search_term).order_by('entry_text')
+    context = {
+        'search_result':search_result,
+        'search_result_length':len(search_result)
+    }
+    return render(request, 'lessons/searchentries.html', context)
 
 def display(request, lesson_id):
-	lesson = get_object_or_404(Lesson, pk = lesson_id)
-	return render(request, 'lessons/display.html', {'lesson':lesson})
+    lesson = get_object_or_404(Lesson, pk = lesson_id)
+    return render(request, 'lessons/display.html', {'lesson':lesson})
+
 
 
 
