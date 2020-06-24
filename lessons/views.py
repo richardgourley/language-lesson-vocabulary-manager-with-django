3c79470d6fe27bef1cf504c82f256fb77be25d77
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-24 18:07:40 +0200

    search_entries updated to render directly after logic

--- lessons/views.py
+++ lessons/views.py
@@ -21,23 +21,22 @@ def all_lessons(request):
     return render(request, 'lessons/all.html', context)
 
 def search_entries(request):
-    search_result = ''
-    message = ''
+    if request.POST and request.POST['entry_search'] == '':
+        return render(request, 'lessons/searchentries.html', {
+            'search_result':None,
+            'message':'Please enter a word or sentence'
+        })
     if request.POST:
-        if request.POST['entry_search'] == '':
-            message = 'Please enter a word or a sentence.'
-        else:
-            search_result = Entry.objects.filter(entry_text__icontains=request.POST['entry_search']).order_by('entry_text')
-            message = "We found 1 matching result" if len(search_result) == 1 else "We found {} matching results.".format(len(search_result))
-    context = {
-        'search_result':search_result,
-        'message':message
-    }
-    return render(request, 'lessons/searchentries.html', context)
+        search_result = Entry.objects.filter(entry_text__icontains=request.POST['entry_search']).order_by('entry_text')
+        message = "We found 1 matching result" if len(search_result) == 1 else "We found {} matching results.".format(len(search_result))
+        return render(request, 'lessons/searchentries.html', {'search_result':search_result, 'message':message})
+    
+    return render(request, 'lessons/searchentries.html', {'search_result':'','message':''})
 
 def display(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk = lesson_id)
     return render(request, 'lessons/display.html', {'lesson':lesson})
+
 
 
 
