b7ca9e76084075d6595f66b83f6ee0df273215a0
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-25 20:48:06 +0200

    Field search_term label changed

--- lessons/forms.py
+++ lessons/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
 class SearchEntryForm(forms.Form):
-	search_term = forms.CharField(label="Enter a search term", max_length=100)
+	search_term = forms.CharField(label="Word or sentence:", max_length=100)
+
