4a929407ee488247b854981115eb9459be1c7ee8
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-25 20:50:37 +0200

    Search entry form and logic added to search_entries

--- lessons/views.py
+++ lessons/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from .models import Lesson, Entry
+from .forms import SearchEntryForm
 
 # Create your views here.
 def index(request):
@@ -21,17 +22,17 @@ def all_lessons(request):
     return render(request, 'lessons/all.html', context)
 
 def search_entries(request):
-    if request.POST and request.POST['entry_search'] == '':
-        return render(request, 'lessons/searchentries.html', {
-            'search_result':None,
-            'message':'Please enter a word or sentence'
-        })
-    if request.POST:
-        search_result = Entry.objects.filter(entry_text__icontains=request.POST['entry_search']).order_by('entry_text')
-        message = "We found 1 matching result" if len(search_result) == 1 else "We found {} matching results.".format(len(search_result))
-        return render(request, 'lessons/searchentries.html', {'search_result':search_result, 'message':message})
-    
-    return render(request, 'lessons/searchentries.html', {'search_result':'','message':''})
+    if request.method == 'POST':
+        form = SearchEntryForm(request.POST)
+        if form.is_valid():
+            search_term = form.cleaned_data['search_term']
+            search_result = Entry.objects.filter(entry_text__icontains=request.POST['search_term']).order_by('entry_text')
+            message = "We found 1 matching result" if len(search_result) == 1 else "We found {} matching results.".format(len(search_result))
+            return render(request, 'lessons/searchentries.html', {'search_result':search_result, 'message':message, 'form':form})
+    else:
+        form = SearchEntryForm()
+            
+    return render(request, 'lessons/searchentries.html', {'search_result':'','message':'', 'form':form})
 
 def display(request, lesson_id):
     lesson = get_object_or_404(Lesson, pk = lesson_id)
