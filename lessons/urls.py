8156e1d685bd52aee4ffe0f49a4e5098a6b81b10
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-26 08:33:23 +0200

    Featured lessons added to urls

--- lessons/urls.py
+++ lessons/urls.py
@@ -5,6 +5,7 @@ app_name = 'lessons'
 urlpatterns = [
     path('', views.index, name="index"),
     path('latest/', views.latest_lessons, name="latest_lessons"),
+    path('featured/', views.featured_lessons, name="latest_lessons"),
     path('viewall/', views.all_lessons, name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
     path('<int:lesson_id>/', views.display, name="display")
