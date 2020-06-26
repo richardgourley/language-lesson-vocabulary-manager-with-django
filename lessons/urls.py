16bdec1429ccef653f6343dcb3b355db79345d75
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-26 21:04:29 +0200

    Latest url removed

--- lessons/urls.py
+++ lessons/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'lessons'
 urlpatterns = [
     path('', views.index, name="index"),
-    path('latest/', views.latest_lessons, name="latest_lessons"),
     path('featured/', views.featured_lessons, name="featured_lessons"),
     path('viewall/', views.all_lessons, name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
