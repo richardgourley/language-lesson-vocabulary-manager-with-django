b34ece64c1c82152067893075f613608a5a73004
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-27 16:00:57 +0200

    FeaturedLessons.as_view() replaces views.featured_lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import IndexView
+from .views import IndexView, FeaturedLessonsView
 
 app_name = 'lessons'
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('featured/', views.featured_lessons, name="featured_lessons"),
+    path('featured/', FeaturedLessonsView.as_view(), name="featured_lessons"),
     path('viewall/', views.all_lessons, name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
     path('<int:lesson_id>/', views.display, name="display")
