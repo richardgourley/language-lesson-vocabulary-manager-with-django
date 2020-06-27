b1d1a611c7d70d0ae59ab244dfea840eecaebbb8
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-27 20:49:18 +0200

    DisplayView.as_view replaces display function

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import IndexView, FeaturedLessonsView, AllLessonsView
+from .views import IndexView, FeaturedLessonsView, AllLessonsView, DisplayView
 
 app_name = 'lessons'
 urlpatterns = [
@@ -8,5 +8,5 @@ urlpatterns = [
     path('featured/', FeaturedLessonsView.as_view(), name="featured_lessons"),
     path('viewall/', AllLessonsView.as_view(), name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
-    path('<int:lesson_id>/', views.display, name="display")
+    path('<int:pk>/', DisplayView.as_view(), name="display")
 ]
