2a3b1596b6c26b9486a8e3189e5d8ca79392f52e
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-27 15:16:25 +0200

    IndexView.as_view() added and called as index page.

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
+from .views import IndexView
 
 app_name = 'lessons'
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', IndexView.as_view(), name="index"),
     path('featured/', views.featured_lessons, name="featured_lessons"),
     path('viewall/', views.all_lessons, name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
