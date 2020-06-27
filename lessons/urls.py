d1cc01f7c197620aa216d23ac2104c3a7857b1bc
richardgourley <39519164+richardgourley@users.noreply.github.com>
2020-06-27 20:45:40 +0200

    AllLessonsView replaces all_lessons

--- lessons/urls.py
+++ lessons/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import IndexView, FeaturedLessonsView
+from .views import IndexView, FeaturedLessonsView, AllLessonsView
 
 app_name = 'lessons'
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
     path('featured/', FeaturedLessonsView.as_view(), name="featured_lessons"),
-    path('viewall/', views.all_lessons, name="all_lessons"),
+    path('viewall/', AllLessonsView.as_view(), name="all_lessons"),
     path('searchentries/', views.search_entries, name="search_entries"),
     path('<int:lesson_id>/', views.display, name="display")
 ]
